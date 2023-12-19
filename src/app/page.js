6320e27d301e353c0300b9a152093c7de07f8f8c
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-12-19 17:45:04 +0300

    add bootstrap

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,17 @@ import styles from './page.module.css'
 import Link from 'next/link'
 
 
-export default function Home() {
+ 
+
+export default  function Home() {
+
+
   return (
-    <main >
-      <Link href="posts">Posts</Link>
-      <h1>123</h1>
-    </main>
+    <main>
+    <Link href="/posts">Posts</Link>
+    <h1>123</h1>
+    
+  </main>
   )
 }
+
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,8 +1,43 @@
 
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+   
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+  }
 
-export default function Posts (){
+export default async function Posts (){
+    const data = await getData()
 
     return(
-        <div><h1>Posts</h1></div>
+        <div>
+            <h1>Posts</h1>
+            {data.length > 0 && data.map(item => (
+      <p key={item.id}>{item.title}</p>
+    ))}
+    <ul class="list-group">
+  <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+</ul>
+        </div>
     )
 }
+
+// export async function getServerSideProps(context)
+// {
+//     console.log(context)
+//     return{
+//       props:{
+//         data:[]
+//       }
+//     }
+// }
