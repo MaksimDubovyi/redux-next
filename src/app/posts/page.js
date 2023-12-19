74f31e72d5c2bb3496a915eb54afae647bdb7395
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-12-19 20:31:25 +0300

    fix Deploymen6

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,29 +1,33 @@
 import { getPosts } from "../services/getPosts"
 
-// async function getData() {
-//      const response =await getPosts()
+async function getData() {
+    
 
+     try {
+        const response =await getPosts()
+        return response
+      }
+     catch (error) {
+        console.error('Error fetching data:', error);
+        throw new Error('Failed to fetch data');
+      }
    
-//     if (!response) {
-//       throw new Error('Failed to fetch data')
-//     }
-   
-//      return response
-//   }
+  
+  }
 
 export default async function Posts (){
-    //  const data = await getData()
+     const data = await getData()
 
     return(
         <div>
             <h1>Posts</h1>
-             {/* <ul className="list-group">
-            {data.length > 0 && data.map(item => (
+             <ul className="list-group">
+            {data &&data.length > 0 && data.map(item => (
                 <li className ="list-group-item" key={item.id}>{item.title}</li>
 
             ))}
          
-            </ul> */}
+            </ul>
 
         </div>
     )
