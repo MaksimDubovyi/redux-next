e14073eac72251acf5b0d6844fc5c5d108f5c3ef
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-12-20 11:51:27 +0300

    fix ur11

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -3,33 +3,33 @@
 /// Cервер ++++ для СЕО
 
 import { getPosts } from "../services/getPosts"
-// import { ItemList } from "./components/ItemList";
+import { ItemList } from "./components/ItemList";
 
 
 
-// async function getData() {
+async function getData() {
     
 
-//      try {
-//         const response =await getPosts()
-//         return response
-//       }
-//      catch (error) {
-//         console.error('Error fetching data:', error);
-//         throw new Error('Failed to fetch data');
-//       }
+     try {
+        const response =await getPosts()
+        return response
+      }
+     catch (error) {
+        console.error('Error fetching data:', error);
+        throw new Error('Failed to fetch data');
+      }
    
   
-//   }
+  }
 
 export default async function Posts (){
-    //  const data = await getData()
+     const data = await getData()
 
     return(
 
         <div>
              <h1>Posts</h1>
-             {/* <ItemList data={data}/> */}
+             <ItemList data={data}/>
         </div>
 
     )
